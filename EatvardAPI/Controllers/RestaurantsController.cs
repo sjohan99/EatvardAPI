dc6d57d9d3b5f225f4d0671353f33a1efaa33d92
Johan Selin <johanselin99@gmail.com>
2022-07-13 01:11:05 +0200

    Added DataAccess as project and moved git repo one level higher

---
+++ EatvardAPI/Controllers/RestaurantsController.cs
@@ -0,0 +1,52 @@
+﻿using EatvardAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EatvardAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantsController : ControllerBase
+    {
+        private readonly EatvardContext _context;
+
+        public RestaurantsController(EatvardContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Restaurant>>> GetRestaurants()
+        {
+            if (_context.Restaurants == null)
+            {
+                return NotFound();
+            }
+            return await _context.Restaurants.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Restaurant>> GetRestaurant(int id)
+        {
+            if (_context.Restaurants == null)
+            {
+                return NotFound();
+            }
+
+            var restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return restaurant;
+        }
+
+        [HttpPut()]
+        public async Task<IActionResult> PutRestaurant(Restaurant restaurant)
+        {
+            return null;
+        }
+    }
+}
