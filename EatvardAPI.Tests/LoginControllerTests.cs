869354bcedb22e0d975c37da8c608457b0089f39
Johan Selin <johanselin99@gmail.com>
2022-07-19 23:55:28 +0200

    Cleaned up LoginController tests by adding constructor

--- EatvardAPI.Tests/LoginControllerTests.cs
+++ EatvardAPI.Tests/LoginControllerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using System.Threading.Tasks;
 using Domain.DTOs;
@@ -14,78 +15,50 @@ namespace EatvardAPI.Tests;
 public class LoginControllerTests
 {
 
-    [Fact]
-    public async Task Login_ShouldReturnUserWithToken()
+    User user;
+    LoginUserDTO loginUserDTO;
+    IUnitOfWork fakeUnitOfWork;
+    LoginController controller;
+
+    public LoginControllerTests()
     {
-        LoginUserDTO loginUserDTO = new LoginUserDTO()
+        loginUserDTO = new LoginUserDTO()
         {
             Email = "test@email.com",
             Password = "password"
         };
 
-        User user = new User()
+        user = new User()
         {
             Email = loginUserDTO.Email,
             PasswordHash = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f", // Correct hash for 'password' with salt '123'
             PasswordSalt = "123"
         };
 
-        var fakeUnitOfWork = A.Fake<IUnitOfWork>();
+        fakeUnitOfWork = A.Fake<IUnitOfWork>();
         A.CallTo(fakeUnitOfWork.Users).WithNonVoidReturnType().Returns(Task.FromResult(user));
-        var controller = new LoginController(fakeUnitOfWork, new JWTUtils("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
-        
-        var actionResult = await controller.Login(loginUserDTO);
-        var result = actionResult.Result as OkObjectResult;
-        var resulting_userDTO = result.Value as UserDTO;
-        Assert.Equal(200, result.StatusCode);
-        Assert.NotNull(resulting_userDTO.JWTToken);
+        controller = new LoginController(fakeUnitOfWork, new JWTUtils("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
     }
 
     [Fact]
     public async Task Login_WrongPasswordShouldReturnUnauthorizedResponse()
     {
-        LoginUserDTO loginUserDTO = new LoginUserDTO()
-        {
-            Email = "test@email.com",
-            Password = "wrongpassword"
-        };
-
-        User user = new User()
-        {
-            Email = loginUserDTO.Email,
-            PasswordHash = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f",
-            PasswordSalt = "123"
-        };
-
-        var fakeUnitOfWork = A.Fake<IUnitOfWork>();
-        A.CallTo(fakeUnitOfWork.Users).WithNonVoidReturnType().Returns(Task.FromResult(user));
-        var controller = new LoginController(fakeUnitOfWork, new JWTUtils("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
+        loginUserDTO.Password = "thisisthewrongpassword";
 
         var actionResult = await controller.Login(loginUserDTO);
         var result = actionResult.Result as UnauthorizedObjectResult;
+
         Assert.Equal(401, result.StatusCode);
     }
 
     [Fact]
-    public async Task Login_ShouldReturnUnauthorizedResponse()
+    public async Task Login_ShouldReturnOkUserWithToken()
     {
-        LoginUserDTO loginUserDTO = new LoginUserDTO()
-        {
-            Email = "test@email.com"
-        };
-        User user = new User()
-        {
-            Email = "also@email.com",
-            PasswordHash = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f",
-            PasswordSalt = "123"
-        };
-        var fakeUnitOfWork = A.Fake<IUnitOfWork>();
-        //A.CallTo(fakeUnitOfWork.Users).WithNonVoidReturnType().Returns(user);
-        //A.CallTo(fakeUnitOfWork.Users).WithNonVoidReturnType().Returns(Task.FromResult(Task.CompletedTask));
-        var controller = new LoginController(fakeUnitOfWork, new JWTUtils("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
-
         var actionResult = await controller.Login(loginUserDTO);
-        var result = actionResult.Result as UnauthorizedObjectResult;
-        Assert.Equal(401, result.StatusCode);
+        var result = actionResult.Result as OkObjectResult;
+        var resulting_userDTO = result.Value as UserDTO;
+
+        Assert.Equal(200, result.StatusCode);
+        Assert.NotNull(resulting_userDTO.JWTToken);
     }
 }
