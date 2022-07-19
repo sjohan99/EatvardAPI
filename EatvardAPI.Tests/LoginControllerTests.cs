9930a42f4384f537a661ca3aecb82efe1afb7806
Johan Selin <johanselin99@gmail.com>
2022-07-19 23:43:35 +0200

    Tests for LoginController added

--- EatvardAPI.Tests/LoginControllerTests.cs
+++ EatvardAPI.Tests/LoginControllerTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Threading.Tasks;
 using Domain.DTOs;
 using EatvardAPI.Controllers;
@@ -12,6 +13,7 @@ namespace EatvardAPI.Tests;
 
 public class LoginControllerTests
 {
+
     [Fact]
     public async Task Login_ShouldReturnUserWithToken()
     {
@@ -24,23 +26,66 @@ public class LoginControllerTests
         User user = new User()
         {
             Email = loginUserDTO.Email,
-            PasswordHash = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f",
+            PasswordHash = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f", // Correct hash for 'password' with salt '123'
             PasswordSalt = "123"
         };
 
-        var fakeUser = A.Dummy<User>();
-        //A.CallTo(() => fakeUser.Email).Returns(loginUserDTO.Email);
-        //A.CallTo(() => fakeUser.PasswordHash).Returns("ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f");
-        //A.CallTo(() => fakeUser.PasswordSalt).Returns("123");
         var fakeUnitOfWork = A.Fake<IUnitOfWork>();
-        //A.CallTo(() => fakeUnitOfWork.Users.FindOneAsync(user => user.Email == loginUserDTO.Email)).Returns(Task.FromResult(fakeUser));
         A.CallTo(fakeUnitOfWork.Users).WithNonVoidReturnType().Returns(Task.FromResult(user));
         var controller = new LoginController(fakeUnitOfWork, new JWTUtils("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
         
         var actionResult = await controller.Login(loginUserDTO);
-
         var result = actionResult.Result as OkObjectResult;
         var resulting_userDTO = result.Value as UserDTO;
+        Assert.Equal(200, result.StatusCode);
         Assert.NotNull(resulting_userDTO.JWTToken);
+    }
+
+    [Fact]
+    public async Task Login_WrongPasswordShouldReturnUnauthorizedResponse()
+    {
+        LoginUserDTO loginUserDTO = new LoginUserDTO()
+        {
+            Email = "test@email.com",
+            Password = "wrongpassword"
+        };
+
+        User user = new User()
+        {
+            Email = loginUserDTO.Email,
+            PasswordHash = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f",
+            PasswordSalt = "123"
+        };
+
+        var fakeUnitOfWork = A.Fake<IUnitOfWork>();
+        A.CallTo(fakeUnitOfWork.Users).WithNonVoidReturnType().Returns(Task.FromResult(user));
+        var controller = new LoginController(fakeUnitOfWork, new JWTUtils("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
+
+        var actionResult = await controller.Login(loginUserDTO);
+        var result = actionResult.Result as UnauthorizedObjectResult;
+        Assert.Equal(401, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task Login_ShouldReturnUnauthorizedResponse()
+    {
+        LoginUserDTO loginUserDTO = new LoginUserDTO()
+        {
+            Email = "test@email.com"
+        };
+        User user = new User()
+        {
+            Email = "also@email.com",
+            PasswordHash = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f",
+            PasswordSalt = "123"
+        };
+        var fakeUnitOfWork = A.Fake<IUnitOfWork>();
+        //A.CallTo(fakeUnitOfWork.Users).WithNonVoidReturnType().Returns(user);
+        //A.CallTo(fakeUnitOfWork.Users).WithNonVoidReturnType().Returns(Task.FromResult(Task.CompletedTask));
+        var controller = new LoginController(fakeUnitOfWork, new JWTUtils("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
+
+        var actionResult = await controller.Login(loginUserDTO);
+        var result = actionResult.Result as UnauthorizedObjectResult;
+        Assert.Equal(401, result.StatusCode);
     }
 }
