ab99fc12af5a2e4f50928e1ad61e1b2958e61347
Johan Selin <johanselin99@gmail.com>
2022-07-20 02:19:20 +0200

    Testing AutoMapper, might use instead of manual conversion between entities and DTOs in the future

---
+++ Domain.Tests/AutoMapperTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.DTOs;
+using EatvardDataAccessLibrary.Models;
+using Xunit;
+
+namespace Domain.Tests;
+public class AutoMapperTests
+{
+    [Fact]
+    public void test()
+    {
+        User user = new()
+        {
+            Id = 1,
+            FirstName = "John",
+            Email = "john@email.com"
+        };
+
+        var config = new MapperConfiguration(cfg => cfg.CreateMap<User, UserDTO>());
+        var mapper = config.CreateMapper();
+        UserDTO uDTO = mapper.Map<UserDTO>(user);
+        
+        Assert.Equal(user.FirstName, uDTO.FirstName);
+        Assert.Equal(user.Email, uDTO.Email);
+        Assert.Equal(user.Id, uDTO.Id);
+        Assert.Null(uDTO.LastName);
+    }
+}
